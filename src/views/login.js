e85c9716d48cb0bd55b1a25676a4ccce9c4d752d
快乐的豆腐 <981079142@qq.com>
2021-05-30 09:45:23 +0800

    添加

--- src/views/login.js
+++ src/views/login.js
@@ -1,12 +1,73 @@
 import React, { Component } from "react";
-import {
-  HashRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
+import "./login.css";
+import http from '../server'
 
-function Login(props) {
-  return <h1>Hello, name</h1>;
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+class Login extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields(async (err, values) => {
+      if (!err) {
+        //let returnObj = await http.post("http://localhost:8081/login", values);
+        let returnObj = await http.post("/login", values);
+        debugger;
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="login-wrapper">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator("username", {
+              rules: [{ required: true, message: "请输入名称！" }],
+            })(
+              <Input
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="请输入名称"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator("password", {
+              rules: [{ required: true, message: "请输入密码！" }],
+            })(
+              <Input
+                prefix={
+                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                type="password"
+                placeholder="请输入密码"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              登录
+            </Button>
+            或者
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              注册
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
 }
 
-export default Login;
+export default Form.create({ name: "normal_login" })(Login);
