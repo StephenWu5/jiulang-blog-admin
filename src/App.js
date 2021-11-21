422a2f6b7740d924e2f99dafb8e5760eeebbb066
Your Name <981079142@qq.com>
2021-11-21 21:45:55 +0800

    新增eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import {
-  HashRouter as Router,
-  Route,
-  Switch
+    HashRouter as Router,
+    Route,
+    Switch
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Login from './views/Login';
@@ -12,65 +12,65 @@ import zhCN from 'antd/es/locale/zh_CN';
 import { ConfigProvider } from 'antd';
 
 class App extends React.Component {
-  // 父组件声明自己支持 context
-  static childContextTypes = {
-    children: PropTypes.array
-  };
-
-  state = {
-    routes: []
-  };
-
-  // 父组件提供一个函数，用来返回相应的 context 对象
-  getChildContext() {
-    return {
-      children: routes[0].children
+    // 父组件声明自己支持 context
+    static childContextTypes = {
+        children: PropTypes.array
     };
-  }
 
-  componentDidMount() {
-    this.setState({
-      routes
-    });
-  }
+    state = {
+        routes: []
+    };
 
-  render() {
-    return (
-      <div className="App">
-        {/* 全局国际化配置支持汉字 */}
-        <ConfigProvider locale={zhCN}>
-          <Router>
-            <Switch>
-              {/*
+    // 父组件提供一个函数，用来返回相应的 context 对象
+    getChildContext() {
+        return {
+            children: routes[0].children
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            routes
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {/* 全局国际化配置支持汉字 */}
+                <ConfigProvider locale={zhCN}>
+                    <Router>
+                        <Switch>
+                            {/*
             路由重定向
           */}
-              <Route path="/login" component={Login}></Route>
-              {routes.map((route, key) => {
-                if (route.exact) {
-                  return (
-                    <MyRoute
-                      key={key}
-                      exact
-                      path={route.path}
-                      component={route.component}
-                    />
-                  );
-                } else {
-                  return (
-                    <MyRoute
-                      key={key}
-                      path={route.path}
-                      component={route.component}
-                    />
-                  );
-                }
-              })}
-            </Switch>
-          </Router>
-        </ConfigProvider>
-      </div>
-    );
-  }
+                            <Route path="/login" component={Login}></Route>
+                            {routes.map((route, key) => {
+                                if (route.exact) {
+                                    return (
+                                        <MyRoute
+                                            key={key}
+                                            exact
+                                            path={route.path}
+                                            component={route.component}
+                                        />
+                                    );
+                                } else {
+                                    return (
+                                        <MyRoute
+                                            key={key}
+                                            path={route.path}
+                                            component={route.component}
+                                        />
+                                    );
+                                }
+                            })}
+                        </Switch>
+                    </Router>
+                </ConfigProvider>
+            </div>
+        );
+    }
 }
 
 export default App;
