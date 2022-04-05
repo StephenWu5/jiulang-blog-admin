006c8efa3c639b7507b90709f594d8233bef0d00
Your Name <981079142@qq.com>
2022-04-05 11:21:08 +0800

    把后端设置cookie的方式改为前端设置。

--- mock/server.js
+++ mock/server.js
@@ -5,12 +5,6 @@ var path = require('path'); //系统路径模块
 var fs = require('fs'); //文件模块
 
 app.all('*', function (req, res) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-    res.header('X-Powered-By', ' 3.2.1');
-    res.header('Content-Type', 'application/json;charset=utf-8');
-
     let { url } = req;
     if (url.startsWith('/mock')) {
         //前面参数使用正则，后面使用字符串
@@ -25,13 +19,13 @@ app.all('*', function (req, res) {
                     message: `查找【${jsonDataName}】出错...`
                 });
             } else {
-                if (url === '/mock/api/login') {
-                    // 登录需要设置cookie
-                    const { data: innerData } = JSON.parse(data);
-                    res.cookie('resc', innerData, {
-                        expires: new Date(Date.now() + 900000000)
-                    });
-                }
+                // if (url === '/mock/api/login') {
+                //     // 登录需要设置cookie
+                //     const { data: innerData } = JSON.parse(data);
+                //     res.cookie('resc', innerData, {
+                //         expires: new Date(Date.now() + 900000000)
+                //     });
+                // }
                 res.send(data);
             }
         });
--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,6 @@
-//const serverName = 'dev'; // 本地环境
-const serverName = 'testing'; // 测试环境
-// const serverName  = 'prod'; // 生产环境
+// let serverName = 'dev'; // 本地环境
+let serverName = 'testing'; // 测试环境
+//let serverName  = 'prod'; // 开发环境
 
 // 动态改变URL前边部分
 export function getUrlPrev(serverName) {
