066684a85f88d1b010e18c52ace4ea95af4385e6
Your Name <981079142@qq.com>
2022-04-14 22:16:00 +0800

    删除了一个环境

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,7 +1,9 @@
 let serverName = 'dev'; // 本地环境
-//let serverName = 'testing'; // 测试环境
 //let serverName  = 'prod'; // 生产环境
 
+// 生产服务器： http://127.0.0.1:8081/
+// 本地数据服务器：http://127.0.0.1:5000/mock
+// 测试环境：没有
 // 动态改变URL前边部分
 export function getUrlPrev(serverName) {
     if (serverName === 'dev') {
@@ -10,10 +12,9 @@ export function getUrlPrev(serverName) {
         // 生产环境
         return '';
         // return 'http://127.0.0.1:8081/';
-        // return 'http://127.0.0.1:5000/mock';
-    } else if (serverName === 'testing') {
-        // 测试环境
-        return '';
+    // } else if (serverName === 'testing') {
+    //     // 测试环境
+    //     return '';
     } else {
         return '';
     }
